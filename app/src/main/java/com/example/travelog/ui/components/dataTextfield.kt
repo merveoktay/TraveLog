f8932d5d14b62fb7056182981efdff21f786f181
Merve OKTAY <merve.oktay@hotmail.com>
2024-01-24 21:39:49 +0300

    constans added

---
+++ app/src/main/java/com/example/travelog/ui/components/dataTextfield.kt
@@ -0,0 +1,2 @@
+package com.example.travelog.ui.components
+
---
+++ app/src/main/java/com/example/travelog/ui/components/passwordTextfield.kt
@@ -0,0 +1,10 @@
+package com.example.travelog.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PasswordTextField(){
+
+    // TextField(value = "", onValueChange = "", modifier = Modifier, trailingIcon = IconButton())
+
+}
